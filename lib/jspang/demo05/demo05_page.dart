1c03a34adae3452205ff8ae077414faa0c07b724
wanggw911 <wangguowen911@gmail.com>
2019-07-19 14:56:32 +0800

    完成KeepAlive的Demo编写

--- lib/jspang/demo05/demo05_page.dart
+++ lib/jspang/demo05/demo05_page.dart
@@ -48,11 +48,57 @@ class _KeepAliveDemoState extends State<KeepAliveDemo> with SingleTickerProvider
         body: TabBarView(
           controller: _controller,
           children: <Widget>[
-            Center(child: Text('1111')),
-            Center(child: Text('2222')),
-            Center(child: Text('3333')),
+            //Center(child: Text('1111')),
+            //Center(child: Text('2222')),
+            //Center(child: Text('3333')),
+            TabbarPage(),
+            TabbarPage(),
+            TabbarPage(),
           ],
         ),
+    );
+  }
+}
+
+class TabbarPage extends StatefulWidget {
+  TabbarPage({Key key}) : super(key: key);
+
+  _TabbarPageState createState() => _TabbarPageState();
+}
+
+//混入AutomaticKeepAliveClientMixin，这是保持状态的关键
+//然后重写wantKeppAlive 的值为true。
+class _TabbarPageState extends State<TabbarPage> with AutomaticKeepAliveClientMixin {
+  int _counter = 0;
+  
+  //重写keepAlive 为ture ，就是可以有记忆功能了。
+  @override
+  bool get wantKeepAlive => true;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Text('点一下加1，点一下加1：'),
+             Text('$_counter', style: Theme.of(context).textTheme.display1)
+           ],
+         ),
+       ),
+       //增加一个悬浮按钮，点击时触发增加方法
+       floatingActionButton: FloatingActionButton(
+         onPressed: _incrementCounter,
+         tooltip: 'Increment',
+         child: Icon(Icons.add),
+       ),
     );
   }
 }
