d13adc9709489d3c176c3b1f850adf83a3feefb1
wanggw911 <wangguowen911@gmail.com>
2019-07-19 11:04:50 +0800

    尝试自定义BartItem

---
+++ lib/jspang/demo01/tabbar_barItem.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+
+// 不是一个 widget，所以还不能用下面的方式来自定义：
+// BottomNavigationBarItem 是一个类，
+
+
+// class MyBarItem extends BottomNavigationBarItem {
+//   const MyBarItem({Key key, this.myIndex, this.selectIndex}) : super(key: key);
+
+//   final _normalColor = Colors.blue;
+//   final _selectColor = Colors.red;
+
+//   final int myIndex;
+//   final int selectIndex;
+
+//   Color _tabbarColor() {
+//     return myIndex == selectIndex ? _selectColor : _normalColor;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBarItem(
+//              icon: Icon(
+//                Icons.pages,
+//                color: _tabbarColor(),
+//              ),
+//              title: Text(
+//                'Pages',
+//                style: TextStyle(color: _tabbarColor()),
+//              ),
+//            );
+//   }
+// }
